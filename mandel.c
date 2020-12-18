a332f366665ace5232df590fc316d4c817675ac2
Ashvin Oli <ashvinoli10@gmail.com>
2020-12-18 15:39:56 +0545

    Panning and Navigation with keyboard keys added

--- mandel.c
+++ mandel.c
@@ -12,9 +12,12 @@ double out_max_x= 2;
 double out_min_x=-2;
 double out_max_y= 2;
 double out_min_y=-2;
+int zoom_forever = 0;
+
 
 int draw(SDL_Renderer **,int);
 double map(double,double ,double, double, double);
+void change_viewport_wrt_mouse(int,int,float,float);
 
 int main(int argc, char *argv[])
 {
@@ -49,63 +52,100 @@ int main(int argc, char *argv[])
     //Factor is a random number that will spice things up for the image.
     int factor = 10;
 
-    //Default value of to_render is true and is set true again when the user draws rectangle on the screen
+    //Default value of to_render is true and is set true again when the user does some action scrolls in or moves the frame
     int to_draw = 1;
-
     //Clear using white color before going inside the loop
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
     SDL_RenderClear(renderer);
 
     //Relative position of mouse_x and mouse_y
     int mouse_x, mouse_y;
-
-    //Mapped mouse_x and mouse_y
-    double mouse_x_mapped ,mouse_y_mapped;
+    // offsets to zoom in or out or move image sidewise
+    float offset_x,offset_y;
     while (!quit){
       while (SDL_PollEvent(&event))
 	  {
-	  if (event.type == SDL_QUIT){
-		quit = 1;
+	  offset_x = (out_max_x - out_min_x);
+	  offset_y = (out_max_y - out_min_y);
+	  SDL_GetMouseState(&mouse_x,&mouse_y);
+	   switch (event.type) {
+	   case SDL_QUIT: 
+	     quit = 1;
+	     break;
+	   case SDL_MOUSEWHEEL:
+	     if(event.wheel.y > 0)
+	       // scroll down
+	       {
+		 printf("\r%-40s","Zooming in on mouse pointer. Wait for image to render!");
+		 fflush(stdout);
+		 offset_x /= 4;
+		 offset_y /= 4;
+		 MAX_ITER += 20;
+	       }else if (event.wheel.y < 0)
+	       // scroll up
+	       {
+		 printf("\r%-40s","Zooming out. Wait for image to render!");
+		 fflush(stdout);
+		 offset_x *=2; 
+		 offset_y *=2;
+		 if (MAX_ITER >= 50) {
+		   MAX_ITER -= 10;		   
+		 }
 
-	  }else if (event.type == SDL_MOUSEWHEEL) {
-	    float offset_x,offset_y;
-	    if(event.wheel.y > 0)
-	      // scroll down
-	      {
-		printf("\r%-40s","Zooming in. Wait for image to render!");
-		fflush(stdout);
-		offset_x = (out_max_x - out_min_x)/4;
-		offset_y = (out_max_y - out_min_y)/4;
-		MAX_ITER += 20;
-	      }else if (event.wheel.y < 0)
-	      // scroll up
-	      {
-		printf("\r%-40s","Zooming out. Wait for image to render!");
-		fflush(stdout);
-		offset_x = (out_max_x - out_min_x)*2;
-		offset_y = (out_max_y - out_min_y)*2;
-		MAX_ITER -= 10;
-	      }
-	    SDL_GetMouseState(&mouse_x,&mouse_y);
-	    double smaller = WIDTH > HEIGHT ? HEIGHT:WIDTH;
-	    double mouse_x_mapped = map(mouse_x,0,smaller, out_min_x,out_max_x); 
-	    double mouse_y_mapped = map(mouse_y,0,smaller, out_min_y,out_max_y);
-	    out_min_x = mouse_x_mapped - offset_x;
-	    out_max_x = mouse_x_mapped + offset_x;
-	    out_min_y = mouse_y_mapped - offset_y;
-	    out_max_y = mouse_y_mapped + offset_y;
-	    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-	    SDL_RenderClear(renderer);
-	    to_draw = 1;
+	       }
+	       change_viewport_wrt_mouse(mouse_x,mouse_y,offset_x,offset_y);
+	       SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+	       SDL_RenderClear(renderer);
+	       to_draw = 1;
+	       break;
+	     case SDL_KEYDOWN:
+	       switch (event.key.keysym.sym)
+		 {
+		 case SDLK_w:
+		 //Move up
+		 //Since y axis is inverted subtracting will take us to upper part of screen
+		   printf("\r%-40s","Moving up. Wait for image to render!");
+		   out_min_y -= offset_y/4;
+		   out_max_y -= offset_y/4;
+		   break;
+		 case SDLK_s:
+		 //Move down
+		   printf("\r%-40s","Moving down. Wait for image to render!");
+		   out_min_y += offset_y/4;
+		   out_max_y += offset_y/4;
+		   break;
+		 case SDLK_a:
+		 //Move left
+		   printf("\r%-40s","Moving Left. Wait for image to render!");
+		   out_min_x -= offset_x/4;
+		   out_max_x -= offset_x/4;
+		   break;
+		 case SDLK_d:
+		 //Move right
+		   printf("\r%-40s","Moving Right. Wait for image to render!");
+		   out_min_x += offset_x/4;
+		   out_max_x += offset_x/4;
+		   break;
+		 case SDLK_SPACE:
+		 //Zoom in
+		   printf("\r%-40s","Zooming in on mouse pointer. Wait for image to render!");
+		   change_viewport_wrt_mouse(mouse_x,mouse_y,offset_x/4,offset_y/4);
+		   break;
+		 }
+		 SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+		 SDL_RenderClear(renderer);
+		 to_draw = 1;
+	       break;
+	   }
 
 	  }
-	}
+
 
 	//Draw pixels on the renderer
 	if (to_draw) {
 	  to_draw = draw(&renderer,factor);
 	  SDL_RenderPresent(renderer);
-	  printf("\r%-40s","Image Rendered! You may now zoom.");
+	  printf("\r%-40s","Image Rendered! You may now zoom or pan.");
 	  fflush(stdout);
 	}
 
@@ -122,7 +162,7 @@ int main(int argc, char *argv[])
 int draw(SDL_Renderer **renderer,int factor){
    for (int x = 0; x < WIDTH; x++) {
       for (int y =0;  y < HEIGHT; y++) {
-      //Transforming and scaling such that origin is center and  radius of 2 around it. Scaling uniformly for both. Y is still inverted.
+      // Mapping the screen with the limits.
 	double smaller = WIDTH > HEIGHT ? HEIGHT:WIDTH;
 	double c_real = map(x,0,smaller, out_min_x,out_max_x); 
 	double c_img = map(y,0,smaller, out_min_y,out_max_y); 
@@ -152,6 +192,17 @@ int draw(SDL_Renderer **renderer,int factor){
       }
    }
    return 0;
+}
+
+void change_viewport_wrt_mouse(int mouse_x,int mouse_y,float offset_x, float offset_y){
+  double smaller = WIDTH > HEIGHT ? HEIGHT:WIDTH;
+  double mouse_x_mapped = map(mouse_x,0,smaller, out_min_x,out_max_x); 
+  double mouse_y_mapped = map(mouse_y,0,smaller, out_min_y,out_max_y);
+  out_min_x = mouse_x_mapped - offset_x;
+  out_max_x = mouse_x_mapped + offset_x;
+  out_min_y = mouse_y_mapped - offset_y;
+  out_max_y = mouse_y_mapped + offset_y;
+
 }
 
 double map(double input_value, double input_min, double input_max, double output_min, double output_max){
